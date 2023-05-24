f9691ab6f5a0a530b6ffa7a09d8d5fe0ff253e2c
Daniel Majewski <daniel@cardverse.pl>
2023-05-24 13:29:57 +0200

    Return test

--- tests/test_providers_digitalocean_config.py
+++ tests/test_providers_digitalocean_config.py
@@ -5,7 +5,7 @@ from cloudproxy.providers.config import set_auth
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 
-# def test_set_auth():
-#     with open(os.path.join(__location__, 'test_user_data.sh')) as file:
-#         filedata = file.read()
-#     assert set_auth("testingusername", "testinguserpassword") == filedata
+def test_set_auth():
+    with open(os.path.join(__location__, 'test_user_data.sh')) as file:
+        filedata = file.read()
+    assert set_auth("testingusername", "testinguserpassword") == filedata
